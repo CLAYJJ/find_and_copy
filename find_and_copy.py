5b756bfb9f6cf078fee73e9419d480f74029f95d
CLAYJJ <1198201587@qq.com>
2020-02-13 16:05:03 +0800

    init

---
+++ find_and_copy.py
@@ -0,0 +1,33 @@
+
+import shutil
+import os
+import sys
+
+''' 打印脚本用法'''
+def usage():
+    print("usage:"+sys.argv[0]+" <source_path> <target_path>")
+
+def copy_files(source, target):
+    shutil.copy(source, target)
+
+
+def get_path(path:str, if_exit=False):
+    if not os.path.exists(path) :
+        if not if_exit:
+            os.makedirs(path)
+        else:
+            print("path: %s is not exists!"% (path))
+            exit()
+
+
+    
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        usage()
+        exit()
+    source = sys.argv[1]
+    target = sys.argv[2]
+    
+    get_path(source, True)
+    get_path(target)
+    copy_files(source, target)
